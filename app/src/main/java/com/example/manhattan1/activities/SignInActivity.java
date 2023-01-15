76dc3ebf82ed163895520929381e4279e733bca2
Sanjeeban <sanjeeban5644@gmail.com>
2023-01-15 21:02:23 +0530

    Added intent to SignInActivity

---
+++ app/src/main/java/com/example/manhattan1/activities/SignInActivity.java
@@ -0,0 +1,16 @@
+package com.example.manhattan1.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.manhattan1.R;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
